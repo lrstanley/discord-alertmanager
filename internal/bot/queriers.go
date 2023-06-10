46eb357b59f6443294e047735c76b16839c4f177
Liam Stanley <me@liamstanley.io>
2023-06-10 00:27:50 -0400

    remove updateButtonComponent
    Signed-off-by: Liam Stanley <me@liamstanley.io>

--- internal/bot/queriers.go
+++ internal/bot/queriers.go
@@ -133,33 +133,3 @@ func componentsHasChild[T string | bool | int64 | float64](components []*disgord
 
 	return v, false
 }
-
-// updateButtonComponent recursively searches through message components (and it's children)
-// for a button with the given ID. If found, it updates the button with the given text, style, etc.
-func updateButtonComponent(components []*disgord.MessageComponent, id, text string, style disgord.ButtonStyle, disabled bool) (ok bool) {
-	for _, comp := range components {
-		if !strings.EqualFold(comp.CustomID, id) {
-			continue
-		}
-
-		if text != "" {
-			comp.Label = text
-		}
-
-		comp.Disabled = disabled
-		comp.Style = style
-		return true
-	}
-
-	for _, comp := range components {
-		if len(comp.Components) < 1 {
-			continue
-		}
-
-		if updateButtonComponent(comp.Components, id, text, style, disabled) {
-			return true
-		}
-	}
-
-	return false
-}
